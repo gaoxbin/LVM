33104ce3ff9ce8dd6b1387f2d3b1309426855088
gaoxbin <122803981@qq.com>
2018-01-07 20:09:48 +0800

    提交第一版代码

---
+++ code/lvm/lvm_info/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+#include "info_handle.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	// FIXME:所有参数目前是固定的，以后需要调整为参数传入
+
+	//
+	info_handle_t *ptrlvm_info_handle = NULL;
+
+	// 1.初始化相关数据结构
+
+	// 2.打开镜像
+
+	// 3.解析镜像
+
+	// 4.打印数据
+
+	// 5.关闭镜像
+
+	return 0;
+}
+
